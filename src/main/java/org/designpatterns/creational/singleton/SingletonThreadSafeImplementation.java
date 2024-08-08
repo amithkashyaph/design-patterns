dc62f5511f9d00b5e5b5699a45a917ea1104f557
amithkashyap <amithkashyaprv@gmail.com>
2024-08-08 19:09:08 +0530

    added initial class for thread safe singleton implementation

---
+++ src/main/java/org/designpatterns/creational/singleton/SingletonThreadSafeImplementation.java
@@ -0,0 +1,8 @@
+package org.designpatterns.creational.singleton;
+
+public class SingletonThreadSafeImplementation {
+
+    private static SingletonThreadSafeImplementation INSTANCE = null;
+
+    private SingletonThreadSafeImplementation(){}
+}
