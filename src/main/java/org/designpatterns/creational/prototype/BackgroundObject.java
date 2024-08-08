6549f10f2e5d9e0e086af7f29ad733538d7ca7ea
amithkashyap <amithkashyaprv@gmail.com>
2024-08-08 22:13:01 +0530

    added initial class for prototype pattern implementation

---
+++ src/main/java/org/designpatterns/creational/prototype/BackgroundObject.java
@@ -0,0 +1,10 @@
+package org.designpatterns.creational.prototype;
+
+public class BackgroundObject implements GraphicalObject {
+
+    private Integer x;
+    @Override
+    public GraphicalObject clone() {
+        return null;
+    }
+}
