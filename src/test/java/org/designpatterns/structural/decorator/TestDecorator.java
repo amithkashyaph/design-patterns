1cbefae7c6818dffde9a48b69f766c6afe285133
amithkashyap <amithkashyaprv@gmail.com>
2024-08-11 00:28:46 +0530

    added test for encryption decorator class

--- src/test/java/org/designpatterns/structural/decorator/TestDecorator.java
+++ src/test/java/org/designpatterns/structural/decorator/TestDecorator.java
@@ -1,4 +1,18 @@
 package org.designpatterns.structural.decorator;
 
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class TestDecorator {
+
+    @Test
+    public void testEncryptionDecoration() {
+        Datasource datasource = new FileDatasource();
+        BaseDecorator encryptDecorator = new EncryptDatasourceDecorator(datasource);
+        String encryptedData = encryptDecorator.read();
+
+        assertEquals(encryptedData, "Reading data decrypted");
+    }
 }
