d32f187169478d9bcc299320edf48c9ca5f49f76
amithkashyap <amithkashyaprv@gmail.com>
2024-08-11 13:49:24 +0530

    added initial bitcoin class forimplementing observer pattern

---
+++ src/main/java/org/designpatterns/behavioural/observer/Bitcoin.java
@@ -0,0 +1,13 @@
+package org.designpatterns.behavioural.observer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Bitcoin {
+
+    private Double price;
+}
