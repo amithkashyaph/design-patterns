75d18ea97ed55ed02fe513b4b1c3dcae1bc33a9b
amithkashyap <amithkashyaprv@gmail.com>
2024-08-12 23:40:35 +0530

    added code in Razorpay sdk for adapter pattern implementation

--- src/main/java/org/designpatterns/structural/adapter/RazorpaySDK.java
+++ src/main/java/org/designpatterns/structural/adapter/RazorpaySDK.java
@@ -1,4 +1,12 @@
 package org.designpatterns.structural.adapter;
 
 public class RazorpaySDK {
+
+    public String pay(String transactionId, String username, String email, Long amount) {
+        return "Making payment " + transactionId + " from " + username + " for amount " + amount;
+    }
+
+    public RazorpayStatus status(String transactionId) {
+        return RazorpayStatus.DONE;
+    }
 }
