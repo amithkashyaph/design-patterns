5ec7ffd56f0212df389bef13a656d8ce75250ba8
amithkashyap <amithkashyaprv@gmail.com>
2024-08-13 23:26:24 +0530

    added user concrete class

--- src/main/java/org/designpatterns/creational/factory/factorymethod/User.java
+++ src/main/java/org/designpatterns/creational/factory/factorymethod/User.java
@@ -1,4 +1,13 @@
 package org.designpatterns.creational.factory.factorymethod;
 
 public class User {
+    private String firstName;
+    private String lastName;
+    private Integer age;
+
+    public User(String firstName, String lastName, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
 }
