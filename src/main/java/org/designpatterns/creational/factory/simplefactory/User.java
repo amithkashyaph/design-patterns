3c9bd1114506087bdeb0f452d72e19406f6fe2d2
amithkashyap <amithkashyaprv@gmail.com>
2024-08-13 23:25:42 +0530

    added abstract user class

--- src/main/java/org/designpatterns/creational/factory/simplefactory/User.java
+++ src/main/java/org/designpatterns/creational/factory/simplefactory/User.java
@@ -1,4 +1,14 @@
 package org.designpatterns.creational.factory.simplefactory;
 
 public abstract class User {
+    private String firstName;
+    private String lastName;
+    private Integer age;
+
+    public User(String firstName, String lastName, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
 }
