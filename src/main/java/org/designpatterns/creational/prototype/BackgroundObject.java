462d90320097182ee8d846e3fb4dc6a4f6a32a8f
amithkashyap <amithkashyaprv@gmail.com>
2024-09-09 23:55:20 +0530

    added background object class

--- src/main/java/org/designpatterns/creational/prototype/BackgroundObject.java
+++ src/main/java/org/designpatterns/creational/prototype/BackgroundObject.java
@@ -1,10 +1,34 @@
 package org.designpatterns.creational.prototype;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
 public class BackgroundObject implements GraphicalObject {
 
     private Integer x;
+    private Integer y;
+    private Integer height;
+    private Integer width;
+    private BackgroundObjectType backgroundObjectType;
+
+    @Setter(AccessLevel.NONE)
+    private List<Integer> pixels = new ArrayList<>();
+
+    public BackgroundObject(Integer x, Integer y, Integer height, Integer width, BackgroundObjectType backgroundObjectType) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        this.backgroundObjectType = backgroundObjectType;
+    }
     @Override
-    public GraphicalObject clone() {
-        return null;
+    public BackgroundObject clone() {
+        return new BackgroundObject(x, y, height, width, backgroundObjectType);
     }
 }
