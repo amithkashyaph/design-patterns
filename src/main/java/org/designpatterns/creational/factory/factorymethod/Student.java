01fed0dd37bcc96efa58583013c569ddde63aafa
amithkashyap <amithkashyaprv@gmail.com>
2024-09-10 23:23:58 +0530

    added Student constructor

--- src/main/java/org/designpatterns/creational/factory/factorymethod/Student.java
+++ src/main/java/org/designpatterns/creational/factory/factorymethod/Student.java
@@ -3,5 +3,8 @@ package org.designpatterns.creational.factory.factorymethod;
 public class Student extends User {
     private Integer psp;
 
-
+    public Student(String firstName, String lastName, Integer age, Integer psp) {
+        super(firstName, lastName, age);
+        this.psp = psp;
+    }
 }
