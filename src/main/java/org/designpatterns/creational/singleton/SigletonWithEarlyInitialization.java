a7372129e10d3d953df88e14752635b66cd4bd6f
amithkashyap <amithkashyaprv@gmail.com>
2024-09-10 23:23:23 +0530

    added Singleton class with early initialization

--- src/main/java/org/designpatterns/creational/singleton/SigletonWithEarlyInitialization.java
+++ src/main/java/org/designpatterns/creational/singleton/SigletonWithEarlyInitialization.java
@@ -1,4 +1,11 @@
 package org.designpatterns.creational.singleton;
 
 public class SigletonWithEarlyInitialization {
+    private static SigletonWithEarlyInitialization INSTANCE = new SigletonWithEarlyInitialization();
+
+    private SigletonWithEarlyInitialization() {}
+
+    public static SigletonWithEarlyInitialization getInstance() {
+        return INSTANCE;
+    }
 }
