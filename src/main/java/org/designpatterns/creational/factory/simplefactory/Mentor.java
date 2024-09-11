9cff3b1378e3003d6a4fdc82f56fd19ed635d03b
amithkashyap <amithkashyaprv@gmail.com>
2024-09-11 23:36:45 +0530

    added Mentor class

--- src/main/java/org/designpatterns/creational/factory/simplefactory/Mentor.java
+++ src/main/java/org/designpatterns/creational/factory/simplefactory/Mentor.java
@@ -1,4 +1,12 @@
 package org.designpatterns.creational.factory.simplefactory;
 
 public class Mentor extends User{
+
+    private Integer noOfSubjects;
+    private Integer noOfMentees;
+    public Mentor(String firstName, String lastName, Integer age, Integer noOfMentees, Integer noOfSubjects) {
+        super(firstName, lastName, age);
+        this.noOfMentees = noOfMentees;
+        this.noOfSubjects = noOfSubjects;
+    }
 }
