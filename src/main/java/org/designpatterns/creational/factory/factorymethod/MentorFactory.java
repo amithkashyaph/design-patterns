59a70d8e7440890c695acbe8167ba4032fa5888a
amithkashyap <amithkashyaprv@gmail.com>
2024-09-11 23:36:58 +0530

    added Mentor factory class

--- src/main/java/org/designpatterns/creational/factory/factorymethod/MentorFactory.java
+++ src/main/java/org/designpatterns/creational/factory/factorymethod/MentorFactory.java
@@ -1,4 +1,8 @@
 package org.designpatterns.creational.factory.factorymethod;
 
-public class MentorFactory implements UserFactory{
+public class MentorFactory implements UserFactory {
+    @Override
+    public User createUser(String firstName, String lastName, Integer age, Integer psp, Integer noOfMentees) {
+        return new Mentor(firstName, lastName, age, noOfMentees);
+    }
 }
