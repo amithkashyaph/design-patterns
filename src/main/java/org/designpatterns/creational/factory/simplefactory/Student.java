ef3a5e2de4d44556a8ec7a33ee4c5f9be5c56c61
amithkashyap <amithkashyaprv@gmail.com>
2024-09-12 19:06:06 +0530

    added student class constructor

--- src/main/java/org/designpatterns/creational/factory/simplefactory/Student.java
+++ src/main/java/org/designpatterns/creational/factory/simplefactory/Student.java
@@ -4,5 +4,9 @@ public class Student extends User {
 
     private Double psp;
     private Integer attendance;
-
+    public Student(String firstName, String lastName, Integer age, Double psp, Integer attendance) {
+        super(firstName, lastName, age);
+        this.psp = psp;
+        this.attendance = attendance;
+    }
 }
