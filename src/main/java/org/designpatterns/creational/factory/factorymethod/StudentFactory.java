ea312e5b3026a62eba14b7dab2be8005d3807c79
amithkashyap <amithkashyaprv@gmail.com>
2024-09-12 19:07:38 +0530

    added student factory class

--- src/main/java/org/designpatterns/creational/factory/factorymethod/StudentFactory.java
+++ src/main/java/org/designpatterns/creational/factory/factorymethod/StudentFactory.java
@@ -1,4 +1,8 @@
 package org.designpatterns.creational.factory.factorymethod;
 
-public class StudentFactory implements UserFactory{
+public class StudentFactory implements UserFactory {
+    @Override
+    public User createUser(String firstName, String lastName, Integer age, Integer psp, Integer noOfMentees) {
+        return new Student(firstName, lastName, age, psp);
+    }
 }
