2490681176b31ab528d3590743323f3c6790aa7b
amithkashyap <amithkashyaprv@gmail.com>
2024-09-14 01:41:43 +0530

    added Email consumer

--- src/main/java/org/designpatterns/behavioural/observer/EmailConsumer.java
+++ src/main/java/org/designpatterns/behavioural/observer/EmailConsumer.java
@@ -1,4 +1,8 @@
 package org.designpatterns.behavioural.observer;
 
-public class EmailConsumer {
+public class EmailConsumer implements Consumer{
+    @Override
+    public void consume(Double price) {
+        System.out.println("Email sent");
+    }
 }
