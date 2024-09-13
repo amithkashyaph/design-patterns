b139c30e8ea5fc3e00f7eb34edc8db821541cfdb
amithkashyap <amithkashyaprv@gmail.com>
2024-09-14 01:42:19 +0530

    added test case for database builder

--- src/test/java/org/designpatterns/creational/builder/TestDatabaseBuilder.java
+++ src/test/java/org/designpatterns/creational/builder/TestDatabaseBuilder.java
@@ -1,11 +1,20 @@
 package org.designpatterns.creational.builder;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestDatabaseBuilder {
 
     @Test
     public void testObjectFieldValue() {
+        Database database = Database.builder()
+                .name("Test builder")
+                .host("localhost")
+                .port(3306)
+                .password("test")
+                .databaseType(DatabaseType.MYSQL)
+                .build();
 
+        Assert.assertTrue(database.getName().equals("Test builder"));
     }
 }
