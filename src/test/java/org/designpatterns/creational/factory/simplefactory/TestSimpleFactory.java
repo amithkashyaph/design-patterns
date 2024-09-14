a90648dec505263c3792aa5c56210cb34450cac5
amithkashyap <amithkashyaprv@gmail.com>
2024-09-15 00:56:17 +0530

    added test case for simple factory

--- src/test/java/org/designpatterns/creational/factory/simplefactory/TestSimpleFactory.java
+++ src/test/java/org/designpatterns/creational/factory/simplefactory/TestSimpleFactory.java
@@ -1,4 +1,15 @@
 package org.designpatterns.creational.factory.simplefactory;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestSimpleFactory {
+
+    @Test
+    public void testObjectCreationViaSimpleFactory() {
+        User student = UserFactory.createUser(UserType.STUDENT, "Amith", "Kashyap", 29, 100D, 100, 0 , 0);
+
+        Assert.assertTrue(student instanceof Student);
+        Assert.assertFalse(student instanceof Mentor);
+    }
 }
