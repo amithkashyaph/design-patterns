51b76fd0412d92f05b481b36a4891c6d8d85e0b9
amithkashyap <amithkashyaprv@gmail.com>
2024-09-15 00:56:01 +0530

    added test case for singleton with early initialization

--- src/test/java/org/designpatterns/creational/singleton/TestSigletonWithEarlyInitialization.java
+++ src/test/java/org/designpatterns/creational/singleton/TestSigletonWithEarlyInitialization.java
@@ -5,5 +5,13 @@ import org.junit.Test;
 
 public class TestSigletonWithEarlyInitialization {
 
+    @Test
+    public void testCreatedObjectsAreSame() {
 
+        SigletonWithEarlyInitialization object1 = SigletonWithEarlyInitialization.getInstance();
+        SigletonWithEarlyInitialization object2 = SigletonWithEarlyInitialization.getInstance();
+
+        Assert.assertEquals("Objects created using singleton pattern should be same", object1, object2);
+
+    }
 }
