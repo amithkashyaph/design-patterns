e6b08f57b940e2dfce8a3ceda4c9ecf9059dbcc8
amithkashyap <amithkashyaprv@gmail.com>
2024-09-15 23:57:33 +0530

    added Mentor class

--- src/main/java/org/designpatterns/creational/factory/factorymethod/Mentor.java
+++ src/main/java/org/designpatterns/creational/factory/factorymethod/Mentor.java
@@ -1,4 +1,10 @@
 package org.designpatterns.creational.factory.factorymethod;
 
 public class Mentor extends User {
+
+    private Integer noOfMentees;
+    public Mentor(String firstName, String lastName, Integer age, Integer noOfMentees) {
+        super(firstName, lastName, age);
+        this.noOfMentees = noOfMentees;
+    }
 }
