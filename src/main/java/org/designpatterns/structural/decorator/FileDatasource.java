8e2595d7be271e699587bd40324fda6dd5c226be
amithkashyap <amithkashyaprv@gmail.com>
2024-09-20 00:15:15 +0530

    implemented file data source

--- src/main/java/org/designpatterns/structural/decorator/FileDatasource.java
+++ src/main/java/org/designpatterns/structural/decorator/FileDatasource.java
@@ -1,4 +1,13 @@
 package org.designpatterns.structural.decorator;
 
-public class FileDatasource {
+public class FileDatasource implements Datasource{
+    @Override
+    public String read() {
+        return "Reading data";
+    }
+
+    @Override
+    public void write(String data) {
+        System.out.println("Writing data " + data);
+    }
 }
